58dd706c2180eca36f7820d38315777605a29d37
swdowia1 <testswzaqwsx@gmail.com>
2022-12-29 16:27:06 +0100

    tabela do projektu

---
+++ Pracownik.sql
@@ -0,0 +1,18 @@
+/****** Object:  Table [dbo].[Pracownik]    Script Date: 29.12.2022 16:28:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Pracownik](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[imie] [nchar](10) NOT NULL,
+	[nazwisko] [nchar](10) NOT NULL,
+	[adres] [nchar](10) NOT NULL,
+ CONSTRAINT [PK_Pracowniks] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
